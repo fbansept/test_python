c1c3114708a2569e1c19b7acae1252e8cf659673
fbansept <bansept.franck@gmail.com>
2024-11-08 11:48:31 +0100

    jeu

--- jeu/balle.py
+++ jeu/balle.py
@@ -3,10 +3,10 @@ import random
 
 class Balle :
 
-    def __init__(self, screen_width, screen_height):
+    def __init__(self, jeu):
         self.rayon = random.randint(5,30)
-        self.x = random.randint(self.rayon, screen_width - self.rayon)
-        self.y = random.randint(self.rayon, screen_height - self.rayon)
+        self.x = random.randint(self.rayon, jeu.screen_width - self.rayon)
+        self.y = random.randint(self.rayon, jeu.screen_height - self.rayon)
         self.vitesse_x = random.randint(1,10)
         self.vitesse_y = random.randint(1,10)
         self.couleur = (random.randint(0,220),
@@ -14,17 +14,19 @@ class Balle :
                         random.randint(0,220))
 
     def dessiner(self, screen) :
+
+        
         pygame.draw.circle(screen,
                             self.couleur , 
                             (self.x, self.y), 
                             self.rayon)
         
-    def deplacement(self, screen_width, screen_height) :
+    def deplacement(self, jeu) :
         self.x += self.vitesse_x
         self.y += self.vitesse_y
 
-        if self.x + self.rayon >= screen_width or self.x  <= self.rayon:
+        if self.x + self.rayon >= jeu.screen_width or self.x  <= self.rayon:
             self.vitesse_x = -self.vitesse_x
 
-        if self.y + self.rayon >= screen_height or self.y <= self.rayon:
+        if self.y + self.rayon >= jeu.screen_height or self.y <= self.rayon:
             self.vitesse_y = -self.vitesse_y
--- jeu/jeu.py
+++ jeu/jeu.py
@@ -2,50 +2,55 @@ import pygame
 import sys
 from balle import Balle
 
+class Jeu :
 
-# Initialiser pygame
-pygame.init()
+    def __init__(self):
 
-# Définir les dimensions de la fenêtre
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Aller-Retour d'une Balle")
+        # Initialiser pygame
+        pygame.init()
 
-# Couleurs
-rouge = (255, 0, 0)
-white = (255, 255, 255)
+        # Définir les dimensions de la fenêtre
+        self.screen_width = 800
+        self.screen_height = 600
+        screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("Aller-Retour d'une Balle")
 
-en_cours = True
-clock = pygame.time.Clock()
+        # Couleurs
+        rouge = (255, 0, 0)
+        white = (255, 255, 255)
 
-
-liste_balle = []
-
-for index in range(100) :
-    liste_balle.append(Balle(screen_width, screen_height))
+        en_cours = True
+        clock = pygame.time.Clock()
 
 
-while en_cours:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            en_cours = False
+        liste_balle = []
 
-    # Remplir l'écran et dessiner la balle
-    screen.fill(white)
+        for index in range(100) :
+            liste_balle.append(Balle())
 
 
-    for balle in liste_balle :
-        balle.dessiner(screen)
-        balle.deplacement(screen_width, screen_height)
-    
+        while en_cours:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    en_cours = False
 
-    # Rafraîchir l'affichage
-    pygame.display.flip()
+            # Remplir l'écran et dessiner la balle
+            screen.fill(white)
 
-    # Contrôler la vitesse de la boucle
-    clock.tick(60)
 
-# Quitter pygame
-pygame.quit()
-sys.exit()
+            for balle in liste_balle :
+                balle.dessiner(screen)
+                balle.deplacement(self.screen_width, self.screen_height)
+            
+
+            # Rafraîchir l'affichage
+            pygame.display.flip()
+
+            # Contrôler la vitesse de la boucle
+            clock.tick(60)
+
+        # Quitter pygame
+        pygame.quit()
+        sys.exit()
+
+Jeu()
